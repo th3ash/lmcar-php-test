b4dcd655f5c97c8b1ca013a4e6492a29d4e5d946
qinyunshang <qyunshang@163.com>
2022-11-19 09:47:03 +0800

    change test

--- src/Service/Log/Log4php.php
+++ src/Service/Log/Log4php.php
@@ -4,7 +4,28 @@ namespace App\Service\Log;
 class Log4php extends LogInterface {
 
     public function __construct() {
-        $this->logger = \Logger::getLogger("Log");
+        \Logger::Configure([
+            'rootLogger' => [
+                'appenders' => ['Log']
+            ],
+            'appenders' => [
+                'Log' => [
+                    'class' => 'LoggerAppenderDailyFile',
+                    'layout' => [
+                        'class' => 'LoggerLayoutPattern',
+                        'params' => [
+                            'conversionPattern' => '[%date] [%logger][%-5level] %message%newline',
+                        ],
+                    ],
+                    'params' => [
+                        'file' => './logs/'.date("Ym").'/log4php_%s.log',
+                        'datePattern' => 'd',
+                    ]
+
+                ]
+            ],
+        ]);
+        $this->logger =  \Logger::getLogger("Log");
     }
 
     public function info($message = '')
--- tests/App/DemoTest.php
+++ tests/App/DemoTest.php
@@ -13,14 +13,24 @@ class DemoTest extends TestCase
 
     public function test_foo()
     {
+        $logger = new AppLogger('log4php');
+
+        $userService = new Demo($logger, new HttpRequest());
+        $foo = $userService->foo();
+
+        $this->assertNotEmpty($foo,"foo 返回数据为空");
     }
 
     public function testGetUserInfo()
     {
         $logger = new AppLogger('log4php');
 
-        $userService = new Demo($logger, new HttpRequest());
-        $userInfo = $userService->get_user_info();
+        try {
+            $userService = new Demo($logger, new HttpRequest());
+            $userInfo = $userService->get_user_info();
+        } catch (\Exception $e) {
+            $this->assertNotTrue(true,"GetUserInfo 调用方法异常".$e->getMessage());
+        }
 
         $this->assertNotEmpty($userInfo,"GetUserInfo 返回数据为空");
 
