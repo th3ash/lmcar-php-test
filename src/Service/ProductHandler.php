f403124355021e5789df10108fe321a338925ab9
zw <2426595849@qq.com>
2022-12-26 21:53:49 +0800

    [develop] 合并git分支 - PriceHandler类 测试与开发.
    [test] change the created_time to timestamp
    
    [test] add sorting and filter
    
    [test] add filter function
    
    [test] Refactoring Code add sorting tools
    
    [test] Refactoring Code add total tools
    
    [test] test sorting  code {1}
    
    [test] test total price{1}

--- src/Service/ProductHandler.php
+++ src/Service/ProductHandler.php
@@ -2,7 +2,85 @@
 
 namespace App\Service;
 
+/**
+ * @Class   ProductHandler
+ * @package App\Service
+ */
 class ProductHandler
 {
 
+    /**
+     * get product list total price
+     *
+     * @param  array  $products  the product list
+     *
+     * @return mixed
+     */
+    public function totalPrice(array $products): int
+    {
+        $listTools = new ListTools($products);
+
+        return $listTools->reduce(function (int $currentPrice, array $products): int {
+            return $currentPrice + (int)$products['price'];
+        },0);
+    }
+
+    /**
+     * to sorting
+     *
+     * @param  array  $products
+     *
+     * @return array
+     */
+    public function listSortingDesc(array $products): array
+    {
+        $listTools = new ListTools($products);
+
+        return $listTools->sortingDesc('price')->toArray();
+    }
+
+    /**
+     * list filter
+     *
+     * @param  array  $products
+     *
+     * @return array
+     */
+    public function listFilter(array $products): array
+    {
+        $listTools = new ListTools($products);
+
+        return $listTools->filter(function(array $product) :bool{
+            return strtolower($product['type']) === 'dessert';
+        })->values()->toArray();
+    }
+
+    /**
+     *
+     *
+     * @param  array  $products
+     *
+     * @return array
+     */
+    public function sortingAndToFilter(array $products): array
+    {
+        $listTools = new ListTools($products);
+
+        return $listTools->sortingDesc('price')->filter(function(array $product) :bool{
+            return strtolower($product['type']) === 'dessert';
+        })->values()->toArray();
+    }
+
+    /**
+     * 转换成时间戳
+     *
+     * @param  array  $products
+     *
+     * @return array|mixed
+     */
+    public function mapToStrToTime(array $products): array
+    {
+        $listTools = new ListTools($products);
+        return $listTools->map(function(array $product):array{ $product['create_at'] = strtotime($product['create_at']); return $product;})->toArray();
+    }
 }
--- tests/Service/ProductHandlerTest.php
+++ tests/Service/ProductHandlerTest.php
@@ -10,59 +10,129 @@ use App\Service\ProductHandler;
  */
 class ProductHandlerTest extends TestCase
 {
-    private $products = [
-        [
-            'id' => 1,
-            'name' => 'Coca-cola',
-            'type' => 'Drinks',
-            'price' => 10,
-            'create_at' => '2021-04-20 10:00:00',
-        ],
-        [
-            'id' => 2,
-            'name' => 'Persi',
-            'type' => 'Drinks',
-            'price' => 5,
-            'create_at' => '2021-04-21 09:00:00',
-        ],
-        [
-            'id' => 3,
-            'name' => 'Ham Sandwich',
-            'type' => 'Sandwich',
-            'price' => 45,
-            'create_at' => '2021-04-20 19:00:00',
-        ],
-        [
-            'id' => 4,
-            'name' => 'Cup cake',
-            'type' => 'Dessert',
-            'price' => 35,
-            'create_at' => '2021-04-18 08:45:00',
-        ],
-        [
-            'id' => 5,
-            'name' => 'New York Cheese Cake',
-            'type' => 'Dessert',
-            'price' => 40,
-            'create_at' => '2021-04-19 14:38:00',
-        ],
-        [
-            'id' => 6,
-            'name' => 'Lemon Tea',
-            'type' => 'Drinks',
-            'price' => 8,
-            'create_at' => '2021-04-04 19:23:00',
-        ],
-    ];
 
+    /**
+     *
+     *
+     * @var ProductHandler
+     */
+    private $productHandler;
+
+    /**
+     *
+     */
+    public function setUp(): void
+    {
+        $this->productHandler = new ProductHandler();
+    }
+
+    private $products = [
+            [
+                'id'        => 1,
+                'name'      => 'Coca-cola',
+                'type'      => 'Drinks',
+                'price'     => 10,
+                'create_at' => '2021-04-20 10:00:00',
+            ],
+            [
+                'id'        => 2,
+                'name'      => 'Persi',
+                'type'      => 'Drinks',
+                'price'     => 5,
+                'create_at' => '2021-04-21 09:00:00',
+            ],
+            [
+                'id'        => 3,
+                'name'      => 'Ham Sandwich',
+                'type'      => 'Sandwich',
+                'price'     => 45,
+                'create_at' => '2021-04-20 19:00:00',
+            ],
+            [
+                'id'        => 4,
+                'name'      => 'Cup cake',
+                'type'      => 'Dessert',
+                'price'     => 35,
+                'create_at' => '2021-04-18 08:45:00',
+            ],
+            [
+                'id'        => 5,
+                'name'      => 'New York Cheese Cake',
+                'type'      => 'Dessert',
+                'price'     => 40,
+                'create_at' => '2021-04-19 14:38:00',
+            ],
+            [
+                'id'        => 6,
+                'name'      => 'Lemon Tea',
+                'type'      => 'Drinks',
+                'price'     => 8,
+                'create_at' => '2021-04-04 19:23:00',
+            ],
+        ];
+
+
+
+    /**
+     *  test get total price
+     */
     public function testGetTotalPrice()
     {
-        $totalPrice = 0;
-        foreach ($this->products as $product) {
-            $price = $product['price'] ?: 0;
-            $totalPrice += $price;
-        }
+        $this->assertEquals(143, $this->productHandler->totalPrice($this->products));
+    }
 
-        $this->assertEquals(143, $totalPrice);
+    /**
+     * test list sorting
+     */
+    public function testListSorting()
+    {
+        $listSortingDesc = $this->productHandler->listSortingDesc($this->products);
+
+        //验证排序后的id
+        $this->assertEquals(3, $listSortingDesc[0]['id']);
+        $this->assertEquals(5, $listSortingDesc[1]['id']);
+    }
+
+    /**
+     * 先从大到小排序, 然后Filter Asserting
+     * test list filter
+     */
+    public function testListFilter()
+    {
+        $listFilter = $this->productHandler->listFilter($this->products);
+
+        //预期有两个 type 等于dessert的商品
+        $this->assertCount(2,$listFilter);
+
+        //预期值
+        $this->assertEquals('Dessert', $listFilter[0]['type']);
+        $this->assertEquals('Dessert', $listFilter[1]['type']);
+    }
+
+    /**
+     * 先从大到小排序, 然后Filter Asserting
+     */
+    public function testSortingAndToFilter()
+    {
+        $sortingAndToFilter = $this->productHandler->sortingAndToFilter($this->products);
+
+        //预期有两个 type 等于dessert的商品
+        $this->assertCount(2,$sortingAndToFilter);
+
+        //验证Dessert
+        foreach ($sortingAndToFilter as $key => $product){
+            $this->assertEquals('Dessert', $sortingAndToFilter[$key]['type']);
+        }
+    }
+
+    /**
+     * 将list的时间转换为时间戳
+     */
+    public function testMapToStrToTime()
+    {
+        $sortingAndToFilter = $this->productHandler->mapToStrToTime($this->products);
+        foreach ($this->products as $key => $product){
+            $this->assertEquals(strtotime($product['create_at']),$sortingAndToFilter[$key]['create_at']);
+        }
     }
 }
