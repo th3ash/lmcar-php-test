94331832d67a4d0022681331c3c337218c65ca8c
zw <2426595849@qq.com>
2022-12-27 15:42:51 +0800

    [test] 补充AppLoggerTest层面测试.

--- tests/Service/AppLoggerTest.php
+++ tests/Service/AppLoggerTest.php
@@ -19,11 +19,11 @@ class AppLoggerTest extends TestCase
     {
         //调用log4php 返回 Log4phpDecorator实例
         $logger = new AppLogger('log4php');
-        $this->assertInstanceOf(Log4phpDecorator::class,$logger->getLogger());
+        $this->assertInstanceOf(Log4phpDecorator::class, $logger->getLogger());
 
         //调用thinklog 返回 ThinkLogDecorator实例
         $logger = new AppLogger('thinklog');
-        $this->assertInstanceOf(ThinkLogDecorator::class,$logger->getLogger());
+        $this->assertInstanceOf(ThinkLogDecorator::class, $logger->getLogger());
     }
 
     /**
@@ -36,4 +36,47 @@ class AppLoggerTest extends TestCase
 
         new AppLogger('emptyKey');
     }
+
+    /**
+     * 测试info
+     */
+    public function testInfo()
+    {
+        //mock预期
+        $log4PhpDecorator = $this->getMockBuilder(Log4phpDecorator::class)->getMock();
+        $log4PhpDecorator->expects($this->once())->method('info')->with($this->equalTo( 'info'));
+
+        //执行
+        $logger = new AppLogger($log4PhpDecorator);
+        $logger->info('info');
+    }
+
+    /**
+     * 测试error
+     */
+    public function testError()
+    {
+        //mock预期
+        $log4PhpDecorator = $this->getMockBuilder(Log4phpDecorator::class)->getMock();
+        $log4PhpDecorator->expects($this->once())->method('error')->with($this->equalTo( 'error'));
+
+        //执行
+        $logger = new AppLogger($log4PhpDecorator);
+        $logger->error('error');
+    }
+
+    /**
+     * 测试debug
+     */
+    public function testDebug()
+    {
+        //mock预期
+        $log4PhpDecorator = $this->getMockBuilder(Log4phpDecorator::class)->getMock();
+        $log4PhpDecorator->expects($this->once())->method('debug')->with($this->equalTo( 'debug'));
+
+        //执行
+        $logger = new AppLogger($log4PhpDecorator);
+        $logger->debug('debug');
+    }
+
 }
